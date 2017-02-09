33a839c1d285d3421e874b8b04bd0bc142d30d6f
Simeon Smith <ssmith@wombatweb.us>
2017-02-09 10:05:13 -0800

    Corrected log.js to us else if instead of if.

--- utils/log.js
+++ utils/log.js
@@ -11,7 +11,7 @@ function debug(req, res, next) {
         console.log(new Date(), req.method, req.url);
         console.log(res.body);
       }
-      if (req.url.match(new RegExp('/api/v1/.*'))) {
+      else if (req.url.match(new RegExp('/api/v1/.*'))) {
         console.log(new Date(), req.method, req.url);
         console.log(req.params.id);
       }
