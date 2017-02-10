2c4f9b47f401a646ba744cebefc84d239df6231b
ssmith-wombatweb <ssmith@wombatweb.us>
2017-02-10 11:25:26 -0800

    Changed to put info in log every time and only console when debug is true. Also added debugMsg class and function to sent undefined values to empty strings.

--- utils/log.js
+++ utils/log.js
@@ -5,10 +5,20 @@ const chalk = require('chalk');
 const time = new Date().toISOString();
 
 module.exports.debug = (info) => {
-  if (process.env.DEBUG === 'true') {
-    const errorLog = './logs/error.log';
 
-    // INFO OBJECT VALUES
+  class debugMsg {
+    constructor(object) {
+      this.logMsg = object.logMsg;
+      this.method = object.method;
+      this.url = object.url;
+      this.ip = object.ip;
+      this.level = object.level;
+    }
+  }
+
+  let msg = new debugMsg(info);
+
+  // INFO OBJECT VALUES
     // {
     //   logMsg: '',
     //   method: '',
@@ -16,19 +26,31 @@ module.exports.debug = (info) => {
     //   ip: '',
     //   level: '',
     // }
+  Object.keys(msg).forEach((key) => {
+    if (msg[key] === undefined) {
+      msg[key] = '';
+    }
+    if (msg.url === '') {
+      msg.url = '\t';
+    }
+  });
 
-    const debugLogMsg = `${time}\t${info.ip}\t${info.method}\t${info.url} \t${info.logMsg}`;
+  const errorLog = './logs/error.log';
+  const debugLogMsg = `${time}\t${msg.ip}\t${msg.method}\t${msg.url} \t${msg.logMsg}`;
 
-    fs.stat(errorLog, (err) => {
-      if (err === null) {
-        fs.appendFile(errorLog, `${debugLogMsg}\n`, (writeErr) => {
-          if (writeErr) throw writeErr;
-        });
-      } else if (err.code === 'ENOENT') {
-        fs.appendFile(errorLog, `TIME\t\tIP\tMETHOD\tURL\t\tMESSAGE\n${debugLogMsg}\n`, (writeErr) => {
-          if (writeErr) throw writeErr;
-        });
-      }
-    });
+  fs.stat(errorLog, (err) => {
+    if (err === null) {
+      fs.appendFile(errorLog, `${debugLogMsg}\n`, (writeErr) => {
+        if (writeErr) throw writeErr;
+      });
+    } else if (err.code === 'ENOENT') {
+      fs.appendFile(errorLog, `TIME\t\tIP\tMETHOD\tURL\t\tMESSAGE\n${debugLogMsg}\n`, (writeErr) => {
+        if (writeErr) throw writeErr;
+      });
+    }
+  });
+
+  if (process.env.DEBUG === 'true') {
+    const debugConsoleMsg = '';
   }
 };
