8808399a5c1dfea4682ea1cff9a67cd1dadb0069
ssmith-wombatweb <ssmith@wombatweb.us>
2017-02-10 17:40:03 -0800

    Added basic beginning unit testing for API routes.

--- test/__api.js
+++ test/__api.js
@@ -1,17 +1,31 @@
 const expect = require('chai').expect;
 const request = require('supertest');
-const server = require('../server.js');
 
+const routes = [
+  '/go/:shortUrl',
+  '/api/v1/urls',
+  '/api/v1/urls/:id',
+];
+
+console.log(routes);
 
 describe('API Route Access', () => {
+  let server;
 
-  afterEach( () => {
+  beforeEach(() => {
+    server = require('../server.js');
+  });
+
+  afterEach(() => {
     server.close();
   });
 
-  it ('Response to /api/v1/urls', (done) => {
-    request(server)
-    .get('/api/v1/urls')
-    .expect(200, done);
+  routes.forEach((route) => {
+    it (`GET Response from ${route}`, (done) => {
+      request(server)
+      .get(route)
+      .expect(200)
+      .end(done);
+    });
   });
 });
