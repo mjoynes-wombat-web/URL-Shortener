70ddcff5c08fe8eccae330b054611fe9f0780c19
ssmith-wombatweb <ssmith@wombatweb.us>
2017-02-25 07:16:16 -0800

    Removed unneeded code and syntax errors. Updated dev dependencies.

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+// Require dependencies.
 const gulp = require('gulp');
 const util = require('debug-tool-express');
 const git = require('gulp-git');
@@ -5,41 +6,46 @@ const gitignore = require('gulp-gitignore');
 const argv = require('yargs').argv;
 const fs = require('fs');
 
-const src = gulp.src('./*').pipe(gitignore());
-const pkg = JSON.parse(fs.readFileSync('./package.json'));
+// Task Global Vars.
+const src = gulp.src('./*').pipe(gitignore());  // Git files without .gitignore files.
+const pkg = JSON.parse(fs.readFileSync('./package.json')); // Package.JSON file contents.
 
-gulp.task('add', () => src.pipe(git.add()));
+gulp.task('add', () => src.pipe(git.add())); // Task to dd all changed files to git.
 
-gulp.task('commit', () => {
-  if (argv.m) {
+gulp.task('commit', () => { // Task to commit all files to get. Passing -m with a string adds a message.
+  if (argv.m) { // If -m is passed use it.
     src.pipe(git.commit(argv.m));
     return `Commit successful with message ${argv.m}`;
-  }
+  } // Otherwise use the default message.
   src.pipe(git.commit('Revisions added.'));
   return 'Commit successful without message.';
 });
 
-gulp.task('updateVer', () => {
+gulp.task('updateVer', () => { // Task to update the version in the package.JSON file.
+  // Run with -r to pass the release type to the update function.
+  // Create the new version with the update ver util using the -r passed to the task.
   const newVer = util.updateVer.updateManual(pkg.version, argv.r);
-  if (newVer !== 'Invalid release type.') {
-    pkg.version = newVer;
+  if (newVer !== 'Invalid release type.') { // If the invalid release type error was not returned.
+    pkg.version = newVer; // Set the version to the new version.
 
-    const updatedPackage = JSON.stringify(pkg, null, '  ');
-    fs.writeFile('./package.json', updatedPackage);
-  } else {
+    const updatedPackage = JSON.stringify(pkg, null, '  '); // Stringify the pkg contents.
+    fs.writeFile('./package.json', updatedPackage); // Write the stringified JSON to the package.json file.
+  } else { // If the invalid release type error was returned throw an error.
     throw new Error('Invalid release type');
   }
 });
 
-gulp.task('addCommit', ['add', 'commit']);
+gulp.task('addCommit', ['add', 'commit']); // Task to add and commit all files. -m can be used to pass a message here.
 
-gulp.task('tag', () => {
-  if (argv.m) {
-    git.tag(pkg.version, argv.m, (err) => { if (err) throw err; });
-    return `Added tag ${pkg.version} with message ${argv.v}.`;
-  } else if (argv.vm) {
+gulp.task('tag', () => { // Task to tag the current state with the version number.
+  if (argv.vm) { // If there was a -vm version message passed use it.
+    // Tag git with the version and the -vm message.
     git.tag(pkg.version, argv.vm, (err) => { if (err) throw err; });
     return `Added tag ${pkg.version} with message ${argv.vm}.`;
+  } else if (argv.m) { // If there was a -m message passed use it.
+    // Tag git with the version and the -m message.
+    git.tag(pkg.version, argv.m, (err) => { if (err) throw err; });
+    return `Added tag ${pkg.version} with message ${argv.m}.`;
   }
   git.tag(pkg.version, `Added version ${pkg.version} tag.`, (err) => { if (err) throw err; });
   return `Added tag ${pkg.version}`;
